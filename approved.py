914f8c1373579e884caa38cb062c0c07c0683823
pwups <aylapmacc@gmail.com>
2025-04-27 14:20:52 +0800

    Update approved.py

--- approved.py
+++ approved.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 
+TOKEN = os.environ.get("TOKEN")
+
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
@@ -16,4 +18,4 @@ async def on_ready():
 async def approve(ctx):
     await ctx.send("_ _\n\n　　　　𝜗 　[**checkpoints required**]( https://discord.gg/SAzqaQuCQA )\n　　　　don't join if not posting yet\n-# _ _　　　⠀  **1w  to  post  ask  4  ext.**　<a:freedom:1350041904099889182> 　୧\n\n_ _")
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+bot.run('TOKEN')
