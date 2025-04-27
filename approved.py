8ae546e68258cc07be2712a8b5179ad1abe92407
sexcmiel <aspacioorbella6@gmail.com>
2025-04-27 11:02:51 +0800

    Create approved.py

---
+++ approved.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.guilds = True
+
+bot = commands.Bot(command_prefix=";", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command(name="a")
+async def approve(ctx):
+    await ctx.send("_ _\n\n　　　　𝜗 　[**checkpoints required**]( https://discord.gg/SAzqaQuCQA )\n　　　　don't join if not posting yet\n-# _ _　　　⠀  **1w  to  post  ask  4  ext.**　<a:freedom:1350041904099889182> 　୧\n\n_ _")
+
+bot.run(os.getenv("DISCORD_TOKEN"))
